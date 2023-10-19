c254f8dac1b036649717bc2e879d4997fc34c13b
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-10-20 01:19:50 +0200

    Fix Chromium Database to run for manifest v3

--- platforms/chromium/app/src/engine/Database.ts
+++ platforms/chromium/app/src/engine/Database.ts
@@ -1,104 +1,126 @@
-class Database {
-    storage: Storage;
-    constructor() {
-        this.storage = window.localStorage;
-    }
+import browser from "webextension-polyfill";
 
+class Database {
     writeTimeSpent(domain: string, timeSpentInSeconds: number) {
-        if (domain.length === 0) {
+        if (domain?.length === 0) {
             return;
         }
         try {
-            const timeSpentObject = this.storage.getItem("timeSpent");
-            if (timeSpentObject && timeSpentObject.length && timeSpentObject !== "{}") {
-                const timeSpentMap = new Map<string, number>(JSON.parse(timeSpentObject));
-                timeSpentMap.set(domain, timeSpentInSeconds);
-                this.storage.setItem("timeSpent", JSON.stringify(Array.from(timeSpentMap.entries())));
-            } else {
-                this.storage.setItem(
-                    "timeSpent",
-                    JSON.stringify(Array.from(new Map([[domain, timeSpentInSeconds]]).entries()))
-                );
-            }
+            browser.storage.local
+                .get("timeSpent")
+                .then((content) => {
+                    if (!!content && Object.keys(content) && Object.keys(content)?.length) {
+                        const timeSpentMap = new Map<string, number>(Object.values(content.timeSpent));
+                        timeSpentMap.set(domain, timeSpentInSeconds);
+                        browser.storage.local.set(
+                            JSON.parse(`{"timeSpent":${JSON.stringify(Array.from(timeSpentMap.entries()))}}`)
+                        );
+                    } else {
+                        browser.storage.local.set(
+                            JSON.parse(`{"timeSpent":${JSON.stringify(Array.from(new Map([]).entries()))}}`)
+                        );
+                    }
+                })
+                .catch((error) => {
+                    console.error("writeTimeSpent.browser.storage.local.get.error: ", error);
+                });
         } catch (exception) {
             console.error(`ERROR: `, exception);
         }
     }
 
-    readTimeSpent(domain?: string): number | Map<string, number> {
-        if (domain !== undefined) {
-            try {
-                const timeSpentObject = this.storage.getItem("timeSpent");
+    readTimeSpent(onResult: (content: number | Map<string, number>) => void, domain?: string) {
+        try {
+            browser.storage.local.get("timeSpent").then((content) => {
                 const timeSpentMap =
-                    timeSpentObject && timeSpentObject !== "{}"
-                        ? new Map<string, number>(JSON.parse(timeSpentObject))
+                    content && Object.keys(content).length
+                        ? new Map<string, number>(Object.values(content.timeSpent))
                         : new Map<string, number>([]);
-                return timeSpentMap.get(domain) || 0;
-            } catch (exception) {
-                console.error("ERROR: ", exception);
-                return 0;
-            }
-        } else {
-            try {
-                const timeSpentObject = this.storage.getItem("timeSpent");
-                const timeSpentMap =
-                    timeSpentObject && timeSpentObject !== "{}"
-                        ? new Map<string, number>(JSON.parse(timeSpentObject))
-                        : new Map<string, number>([]);
-                return timeSpentMap;
-            } catch (exception) {
-                console.error("ERROR: ", exception);
-                return new Map<string, number>([]);
-            }
+                onResult(domain ? timeSpentMap.get(domain) || 0 : timeSpentMap);
+            });
+        } catch (exception) {
+            console.error("ERROR: ", exception);
+            return 0;
         }
     }
 
     writePreviousDomain(domain: string) {
         try {
-            this.storage.setItem("previousDomain", domain);
+            browser.storage.local.set(JSON.parse(`{"previousDomain":"${domain}"}`)).catch((error) => {
+                console.error("writePreviousDomain.set.error: ", error);
+            });
         } catch (exception) {
             console.error("ERROR: ", exception);
         }
     }
 
-    readPreviousDomain(): string {
+    readPreviousDomain(onResult: (domain: string) => void) {
         try {
-            return this.storage.getItem("previousDomain") || "";
+            browser.storage.local
+                .get("previousDomain")
+                .then((content) => {
+                    onResult(content.previousDomain);
+                })
+                .catch((error) => {
+                    console.error("readPreviousDomain.browser.storage.local.get.error: ", error);
+                    onResult("");
+                });
         } catch (exception) {
             console.error("ERROR: ", exception);
-            return "";
+            onResult("");
         }
     }
 
     writeLastActive(domain: string, date: Date) {
         try {
-            const lastActiveObject = this.storage.getItem("lastActive");
-            if (lastActiveObject && lastActiveObject.length && lastActiveObject !== "{}") {
-                const lastActiveMap = new Map<string, Date>(JSON.parse(lastActiveObject));
-                lastActiveMap.set(domain, date);
-                this.storage.setItem("lastActive", JSON.stringify(Array.from(lastActiveMap.entries())));
-            } else {
-                this.storage.setItem(
-                    "lastActive",
-                    JSON.stringify(Array.from(new Map<string, Date>([[domain, date]]).entries()))
-                );
-            }
+            browser.storage.local
+                .get("lastActive")
+                .then((lastActiveObject) => {
+                    if (lastActiveObject && Object.keys(lastActiveObject).length) {
+                        const content = new Map<string, Date>(Object.values(lastActiveObject.lastActive));
+                        content.set(domain, date);
+                        browser.storage.local
+                            .set(JSON.parse(`{"lastActive":${JSON.stringify(Array.from(content.entries()))}}`))
+                            .catch((error) => {
+                                console.error("browser.storage.local.set.error: ", error);
+                            });
+                    } else {
+                        browser.storage.local
+                            .set(
+                                JSON.parse(
+                                    `{"lastActive":${JSON.stringify(
+                                        Array.from(new Map<string, Date>([[domain, date]]).entries())
+                                    )}}`
+                                )
+                            )
+                            .catch((error) => {
+                                console.error("browser.storage.local.set.error: ", error);
+                            });
+                    }
+                })
+                .catch((error) => {
+                    console.error("writeLastActive.ERROR:", error);
+                });
         } catch (exception) {
-            console.error("ERROR: ", exception);
+            console.error(`ERROR: `, exception);
         }
     }
 
-    readLastActive(domain: string): Date {
+    readLastActive(domain: string, onReturn: (date: Date) => void) {
+        if (domain.length === 0) {
+            return;
+        }
         try {
-            const lastActiveObject = this.storage.getItem("lastActive");
-            const lastActiveMap =
-                lastActiveObject && lastActiveObject !== "{}"
-                    ? new Map<string, Date>(JSON.parse(lastActiveObject))
-                    : new Map<string, Date>([]);
-            return new Date(lastActiveMap.get(domain) || new Date());
+            browser.storage.local.get("lastActive").then((content) => {
+                const lastActiveMap =
+                    content && Object.keys(content).length
+                        ? new Map<string, Date>(Object.entries(content.lastActive))
+                        : new Map<string, Date>([]);
+                onReturn(new Date(lastActiveMap.get(domain) || new Date()));
+            });
         } catch (exception) {
             console.error("ERROR: ", exception);
-            return new Date();
+            onReturn(new Date());
         }
     }
 }
