0d5a93590f53b83d0e630c9a74dc24bd02e2ebbd
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-11-10 16:46:54 +0100

    Make sorting select display optionally in DurationHeader

--- app/src/popup/subcomponents/DurationHeader.tsx
+++ app/src/popup/subcomponents/DurationHeader.tsx
@@ -5,14 +5,14 @@ import {Sort} from "../Utils";
 
 interface Props {
     isExpanded: boolean;
-    onSortSelected: (option: Sort) => void;
+    onSortSelected?: (option: Sort) => void;
 }
 
 export const DurationHeader = ({isExpanded, onSortSelected}: Props) => {
     return (
         <div className="duration-header">
             <div className="duration-header-text">{translate("duration-header")}</div>
-            {isExpanded ? <SortingList onSelect={onSortSelected} /> : null}
+            {isExpanded && !!onSortSelected ? <SortingList onSelect={onSortSelected} /> : null}
         </div>
     );
 };
