b6f8d2af90e945c3c574f2e41df391048df39dc9
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2024-01-17 19:21:03 +0100

    Navigate to details directly from ShrinkedView

--- app/src/popup/subcomponents/ShrinkedView.tsx
+++ app/src/popup/subcomponents/ShrinkedView.tsx
@@ -3,6 +3,8 @@ import {getActiveTabDomainFromURL, getHours, getMinutes, getSeconds, getWebsiteI
 import browser from "webextension-polyfill";
 import Database from "../../engine/Database";
 import DurationHeader from "./DurationHeader";
+import Button from "./Button";
+import DetailsView from "./DetailsView";
 
 const getCurrentTimeForCurrentDomain = (domain: string, onResult: (content: number | Map<string, number>) => void) => {
     const db = new Database();
@@ -13,6 +15,7 @@ export const ShrinkedView = () => {
     const [icon, setIcon] = useState<Icon>(getWebsiteIconObject(""));
     const [timeInSeconds, setTimeInSeconds] = useState(0);
     const [activeDomain, setActiveDomain] = useState("");
+    const [detailsScreenWebsite, setDetailsScreenWebsite] = useState<string | null>();
 
     useEffect(() => {
         browser.tabs
@@ -47,17 +50,22 @@ export const ShrinkedView = () => {
 
     return (
         <>
-            <DurationHeader />
-            <div className="shrinked-view-container">
-                <div className="shrinked-view-icon-container">
-                    <img className="shrinked-view-icon" width={icon.size} height={icon.size} src={icon.src} />
-                </div>
-                <div className="shrinked-view-time-spent-text-container">
-                    <div className="shrinked-view-time-spent-text">
-                        {getHours(timeInSeconds)}:{getMinutes(timeInSeconds)}:{getSeconds(timeInSeconds)}
+            {!detailsScreenWebsite ? <DurationHeader /> : null}
+            {detailsScreenWebsite ? (
+                <DetailsView website={detailsScreenWebsite} onBackButtonClick={() => setDetailsScreenWebsite(null)} />
+            ) : (
+                <div className="shrinked-view-container">
+                    <div className="shrinked-view-icon-container">
+                        <img className="shrinked-view-icon" width={icon.size} height={icon.size} src={icon.src} />
                     </div>
+                    <div className="shrinked-view-time-spent-text-container">
+                        <div className="shrinked-view-time-spent-text">
+                            {getHours(timeInSeconds)}:{getMinutes(timeInSeconds)}:{getSeconds(timeInSeconds)}
+                        </div>
+                    </div>
+                    <Button label="details-button-label" onClick={() => setDetailsScreenWebsite(activeDomain)} />
                 </div>
-            </div>
+            )}
         </>
     );
 };
